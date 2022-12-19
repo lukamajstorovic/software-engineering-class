f48ad84959d2cdb913bbbf935792c5ff9e2cf25b
lukamajstorovic <lmajstorovic@etfos.hr>
2022-12-19 13:16:27 +0100

    Add images to /images

--- lab6/myimgur/app/views.py
+++ lab6/myimgur/app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Image
+
 # Create your views here.
 
 def home(request):
     return render(request, 'app/home.html', {})
 
 def index(request):
-    return HttpResponse("RADI!")
+    images = Image.objects.all()
+    context = {
+        'image_list': images,
+    }
+    return render(request, 'app/index.html', context)
