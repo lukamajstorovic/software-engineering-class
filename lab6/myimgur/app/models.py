8ec2f45a59513ce329868baa50d7ff463f349223
lukamajstorovic <lmajstorovic@etfos.hr>
2022-12-19 12:14:42 +0100

    Add models

--- lab6/myimgur/app/models.py
+++ lab6/myimgur/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    url = models.CharField(max_length=256)
+    title = models.CharField(max_length=128, unique=True)
+    pub_date = models.DateTimeField('Pubished at')
+
+    def __str__(self):
+        return f"{self.id}: {self.title[:30]}"
+
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    text = models.TextField(blank=False)
+
+    def __str__(self):
+        return f"{self.id}: ({self.image.title[:15]}) {self.text[:30]}"
