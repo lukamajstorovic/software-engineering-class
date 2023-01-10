869d1d8732f3c4ea47f36525d097bcb14e37c3fa
levara <hrvoje.leventic@gmail.com>
2023-01-10 14:48:21 +0100

    Add lab7

---
+++ lab7/myimgur/app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Image
+
+# Create your views here.
+def home(request):
+    context = {}
+    return render(request, 'app/home.html', context)
+
+def index(request):
+    images = Image.objects.all()
+    context = {
+        'image_list': images
+    }
+    return render(request, 'app/index.html', context)
+
+def detail(request, image_id):
+    image = get_object_or_404(Image, pk=image_id)
+    context = {
+        'image': image
+    }
+    return render(request, 'app/detail.html', context)
