6bb57f7d9c5342d2bf2056d2731b666e0917d149
levara <hrvoje.leventic@gmail.com>
2023-01-23 14:09:51 +0100

    Add lab8 for PRP1 group

---
+++ lab8-prp1/myimgur/app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    url = models.CharField(max_length=256, blank=False)
+    title = models.CharField(max_length=128, blank=False)
+    pub_date = models.DateTimeField('Published at')
+    def __str__(self):
+        return f"{self.id}-{self.title[:20]}"
+    
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    text = models.TextField(blank=False)
+    pub_date = models.DateTimeField('Published at')
+    nick = models.CharField(max_length=128, blank=False, default='anon')
+    approved = models.BooleanField(default=False) 
+    def __str__(self):
+        return f"{self.id}-{self.image.title[:15]}-{self.text}"
+    
---
+++ lab8-prp1/myimgur/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "app"
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('<int:image_id>', views.detail, name="detail"),
+    path('<int:image_id>/comment', views.post_comment, name="post_comment"),
+    path('comments/<int:comment_id>/approve', views.approve_comment, name="approve_comment")
+]
